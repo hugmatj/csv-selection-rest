b71097849122f0ef32bc7a1dc2dad98beabcdd5e
massoudTheDude <mahermassoud@gmail.com>
2016-03-03 11:31:51 -0800

    Begin implementation of MatrixReader, init, remove_duplicates and part of get_submatrix

--- MatrixReader.py
+++ MatrixReader.py
@@ -12,14 +12,18 @@ class MatrixReader(object):
 
     datasets = None
 
-    def __init__(self, dataset_map):
+    def __init__(self, dataset_map, sep_char='\t'):
         """
         Reads in csv files and saves pandas dataframes to memory
 
         :param dataset_map: Dictionary where keys are dataframe names,
             and values are filepaths to their csv files
+        :param sep_char: Character used to separate elements in data, tab by default
         """
+        for name in dataset_map:
+            self.datasets[name] = pd.read_csv( dataset_map[name], sep=sep_char, index_col=0)
 
+    # TODO Maybe change to static method or remove self param
     def __remove_duplicates(self, query_rows, query_columns):
         """
         Removes duplicates from rows and columns query
@@ -29,6 +33,8 @@ class MatrixReader(object):
         :return: tuple where first element is query rows, 2nd is query columns,
             both without duplicates
         """
+        de_duped_query = (set(query_rows), set(query_columns))
+        return de_duped_query
 
     def get_submatrix(self, query_rows, query_columns, dataset_name):
         """
@@ -41,6 +47,17 @@ class MatrixReader(object):
             exactly the same as dataset name in datasets instance variable
         :return: Pandas dataframe of only queried rows and columns
         """
+        # Get table we are querying
+        table = self.datasets[dataset_name]
+
+        """
+        Loop through all queries. If it is contained in table, append to
+        output, otherwise, skip it
+        """
+        for row in query_rows:
+            if row in table.index:
+                # Add row and respective columns to output
+            else:
 
     def get_json(self, query_rows, query_columns, dataset_name):
         """
